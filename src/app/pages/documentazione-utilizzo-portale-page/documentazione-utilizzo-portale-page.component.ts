9ef1d2b8713feb1a61f53cc2f568c37356d8d172
Mirko Rossi <m.rossi@rjcsoft.it>
2021-04-01 17:08:52 +0200

    [PTT-56] FE Configurazione tributo: Imposta gli enti, Definisci le rate

--- src/app/pages/documentazione-utilizzo-portale-page/documentazione-utilizzo-portale-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-documentazione-utilizzo-portale-page',
-  templateUrl: './documentazione-utilizzo-portale-page.component.html',
-  styleUrls: ['./documentazione-utilizzo-portale-page.component.sass']
-})
-export class DocumentazioneUtilizzoPortalePageComponent {}
