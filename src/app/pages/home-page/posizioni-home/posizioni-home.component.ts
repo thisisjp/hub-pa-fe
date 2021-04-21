12a6d865addf410628484a9c274beae84d7e34d0
Antonio Scibilia <a.scibilia@rjcsoft.it>
2021-04-21 18:30:50 +0200

    partial commit

--- src/app/pages/home-page/posizioni-home/posizioni-home.component.ts
+++ src/app/pages/home-page/posizioni-home/posizioni-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Position } from 'src/app/models/position';
 
 @Component({
   selector: 'app-posizioni-home',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posizioni-home.component.sass']
 })
 export class PosizioniHomeComponent implements OnInit {
+  listPosition: Array<Position> = [
+    {
+      id: 2,
+      fiscalCode: 'RSSSMRSRTS',
+      name: 'Mario',
+      surname: 'Rossi',
+      notifyCode: '0000000000000000',
+      date: new Date(),
+      status: 3
+    }
+  ];
+
   constructor() {
     //
   }
