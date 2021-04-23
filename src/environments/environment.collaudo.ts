f87e724f459b035ce8c859587ee7b57d6cf1791d
Mirko Rossi <m.rossi@rjcsoft.it>
2021-04-23 12:04:15 +0200

    [PTT-57] add prefix for microservices url

--- src/environments/environment.collaudo.ts
+++ src/environments/environment.collaudo.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   environmentName: 'COLLAUDO',
-  API_URL: ''
+  API_URL: '',
+  cvsMaxRows: 500,
+  PREFIX_URL_ENTE: '',
+  PREFIX_URL_SERVICE_MANAGEMENT: '',
+  PREFIX_URL_UPLOAD_PAYMENTS: ''
 };
