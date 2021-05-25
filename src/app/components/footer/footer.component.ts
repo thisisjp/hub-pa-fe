6e0b0da5a9986ac194a09e30872cbceda555a712
Mirko Rossi <m.rossi@rjcsoft.it>
2021-05-25 11:23:44 +0200

    [PTT-158] navigation footer privacy

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.sass']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  @ViewChild('modalPrivacy') modalPrivacy: any;
+
+  openModal(): void {
+    this.modalPrivacy.nativeElement.click();
+  }
+}
