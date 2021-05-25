177bf884ff391824efe9f64aa572d72153aca4bf
Mirko Rossi <m.rossi@rjcsoft.it>
2021-05-25 12:25:42 +0200

    [PTT-180] navigation not found page

--- src/app/pages/not-found-page/not-found-page.component.ts
+++ src/app/pages/not-found-page/not-found-page.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Menu } from '../../models/enums/menu.enum';
 
 @Component({
   selector: 'app-not-found-page',
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.sass']
 })
-export class NotFoundPageComponent {}
+export class NotFoundPageComponent {
+  menuEnum = Menu;
+
+  constructor(private router: Router) {}
+
+  goToPage(path: string): void {
+    this.router.navigate([path]).catch(reason => reason);
+  }
+}
