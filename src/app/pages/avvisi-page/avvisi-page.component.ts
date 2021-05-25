2294fadba06e62cb70772e5a8ed0c98399fcd6ad
Mirko Rossi <m.rossi@rjcsoft.it>
2021-05-25 09:28:04 +0200

    [PTT-176] navigation upload csv

--- src/app/pages/avvisi-page/avvisi-page.component.ts
+++ src/app/pages/avvisi-page/avvisi-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Menu } from '../../models/enums/menu.enum';
+import { ServiceManagementService } from '../../services/service-management.service';
+import { TokenService } from '../../services/token.service';
 
 @Component({
   selector: 'app-avvisi-page',
@@ -10,9 +12,19 @@ import { Menu } from '../../models/enums/menu.enum';
 export class AvvisiPageComponent implements OnInit {
   private menuEnum = Menu;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private serviceManagementService: ServiceManagementService,
+    private tokenService: TokenService
+  ) {}
 
   ngOnInit(): void {
-    this.router.navigate([this.menuEnum.AVVISI_PATH + '/' + this.menuEnum.AVVISI_STEP1]).catch(reason => reason);
+    this.serviceManagementService.isServiceConfigurated(this.tokenService.getFiscalCode()).subscribe(res => {
+      if (res && res.result) {
+        this.router.navigate([this.menuEnum.AVVISI_PATH + '/' + this.menuEnum.AVVISI_STEP1]).catch(reason => reason);
+      } else if (res && !res.result) {
+        this.router.navigate([this.menuEnum.HOME_PATH + '/' + this.menuEnum.TRIBUTI_STEP0]).catch(reason => reason);
+      }
+    });
   }
 }
